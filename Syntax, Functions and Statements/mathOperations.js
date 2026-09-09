a0cd9e479ef821bb6b90ddc846592486c5978114
BozhidarTsvetanov <bcvetanov23@gmail.com>
2026-09-09 22:15:17 +0300

    Create mathOperations.js

---
+++ Syntax, Functions and Statements/mathOperations.js
@@ -0,0 +1,28 @@
+function mathOperations(num1, num2, str) {
+
+    let result = 0
+
+    if (str === '+') {
+        result = num1 + num2
+
+    } else if (str === '-') {
+        result = num1 - num2
+
+    } else if (str === '*') {
+        result = num1 * num2
+
+    } else if (str === '/') {
+        result = num1 / num2
+
+    } else if (str === '%') {
+        result = num1 % num2
+        
+    } else if (str === '**') {
+        result = num1 ** num2
+        
+    }
+    console.log(result);
+    
+    
+}
+mathOperations(5, 6, '+')
