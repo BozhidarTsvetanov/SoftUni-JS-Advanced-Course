dd22ad36d9ae7934f84a3aa05ba5402aa0fb060a
BozhidarTsvetanov <bcvetanov23@gmail.com>
2026-09-09 16:13:30 +0300

    Create stringLength.js

---
+++ Syntax, Functions and Statements/stringLength.js
@@ -0,0 +1,8 @@
+function stringLength(str1, str2, str3) {
+    let length = str1.length + str2.length + str3.length
+    console.log(length);
+    let average = Math.floor(length/3)
+    console.log(average);
+    
+}
+stringLength('chocolate', 'ice cream', 'cake')
